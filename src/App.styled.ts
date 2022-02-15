a5b7ba61850b8c2b27b740fd3c93cc46993eafde
ThibaudFabvre <thibaudfabvre@gmail.com>
2022-02-15 06:55:46 +0100

    feat: TT-02 add styled-componentsand remove unused files

---
+++ src/App.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+    background-color: red;
+    height: 100vh;
+    width: 100vw;
+`;
+
+export const TestText = styled.p``;
